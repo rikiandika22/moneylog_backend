27e6678269823303b9f0abb224e3fe237b7aa796
riki andika khusna saputra <andikasan765@gmail.com>
2026-06-01 15:56:17 +0700

    fix backend crash after cors update

--- src/app.js
+++ src/app.js
@@ -5,13 +5,19 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-  origin: '*',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json());
 
